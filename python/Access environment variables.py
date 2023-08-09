e35c3defe458bac2ce6777ae9ff1d4f8664bdc6c
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-08-09 13:41:39 +0300

    Add files via upload

---
+++ python/Access environment variables.py
@@ -0,0 +1,12 @@
+import os
+
+# os.environ gives us system variable information
+print(os.environ)
+
+print('#######################################################')
+
+# PATH is used to specify the paths in which the system searches for program or command
+
+print(os.environ['PATH'])
+
+print('########################################################')
