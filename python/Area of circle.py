fee3e738ba8a2aa381437b2c4d3f95b5b0dea672
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-08-09 13:52:03 +0300

    Add files via upload

---
+++ python/Area of circle.py
@@ -0,0 +1,9 @@
+# math to call pi
+import math
+
+#input the radious of circle
+r = float(input ("Input the radius of the circle : "))
+#calculate the area of circle
+
+print ("The area of the circle with radius " , math.pi * r**2)
+#######################################################
