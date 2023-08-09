735e61286bdb1b3ab1a1fa615445bb709aedadf7
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-08-09 13:32:27 +0300

    Add files via upload

---
+++ python/vowel or not.py
@@ -0,0 +1,11 @@
+letter= input("enter your letter: ") ##input letter you want test
+
+#list of vowel
+vowel= ['a','i','o','u','e','A','I','O','U','E']
+
+#if condition to check the letter is vowel or not
+if letter in vowel:
+    print("letter is vowel")
+    
+else:
+    print("is not ")
