fd87cc13e03b1d8d07457d68444e4038af3a6040
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-08-24 14:58:11 +0300

    Create main.py

---
+++ python/week2/firefox/main.py
@@ -0,0 +1,15 @@
+import Firefox
+
+x=None
+x=input()
+if x == "facebook":
+    Firefox.facebook()
+     
+
+if x == "google":
+    Firefox.google()
+     
+pass
+
+if x=="python":
+    Firefox.python()
